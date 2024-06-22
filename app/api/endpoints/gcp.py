ea9fd3ec1c932a29ff64f3c7a7ee4e9e3bda7ee8
PinsaraPerera <1pawanpinsara@gmail.com>
2024-06-22 23:19:55 +0530

    added core configurations to cloud buckets and generate signed URL for file uploading

--- app/api/endpoints/gcp.py
+++ app/api/endpoints/gcp.py
@@ -20,3 +20,7 @@ def upload_file(params: gcp_schema.StorageBase, request: Request):
 @router.post("/setupVectorStore")
 def setup_vector_store(params: gcp_schema.StorageBase, request: Request):
     return gcp.setup_vector_store(params, DATA_FOLDER, VECTOR_STORE_FOLDER)
+
+@router.post("/generateSignedUrl")
+def generate_signed_url(params: gcp_schema.StorageCreate, request: Request):
+    return gcp.generate_signed_url(params, DATA_FOLDER)
