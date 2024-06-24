50f99d53e73e90a6de9a07f71913ca57a8384363
PinsaraPerera <1pawanpinsara@gmail.com>
2024-06-24 13:03:47 +0530

    optimized the prompts

--- app/core/config.py
+++ app/core/config.py
@@ -39,12 +39,72 @@ question :
 {question}
 """
 
-graph_system_template = """
+graph_system_hard_template = """
 You are a graph generating bot. You have access to the scenario as a text in context. You need to provide the 
 mermaid graph notation as a text for the given scenario. User will provide the scenario then you need to get the best possible graph for the context.
 Just reply to the user without any formatting. Use above information as a reference to provide the best possible graph representation for the user.
 
-graph notation should be a text starting from graph TD and ending with a semicolon.
+you have freedom to choose the best possible visualizations for the given scenario out of below options:
+
+1. Flowchart
+2. Sequence Diagram
+3. Class Diagram
+4. State Diagram
+5. Entity Relationship Diagram
+6. Gantt
+7. Pie Chart
+8. Quadrant Chart
+9. Requirement Diagram
+10. Mindmaps
+11. Timeline
+12.XYChart
+13.Block Diagram
+
+consider user is asking for a complex visual representation for the given scenario and have a good technical knowledge to understand the complex visual representation.
+
+"""
+
+graph_system_medium_template = """
+You are a graph generating bot. You have access to the scenario as a text in context. You need to provide the 
+mermaid graph notation as a text for the given scenario. User will provide the scenario then you need to get the best possible graph for the context.
+Just reply to the user without any formatting. Use above information as a reference to provide the best possible graph representation for the user.
+
+you have freedom to choose the best possible visualizations for the given scenario out of below options:
+
+1. Flowchart
+2. Sequence Diagram
+3. Class Diagram
+4. State Diagram
+6. Gantt
+7. Pie Chart
+8. Quadrant Chart
+9. Requirement Diagram
+10. Mindmaps
+11. Timeline
+12.XYChart
+12.Block Diagram
+
+consider user is asking for a medium complex graph representation have some basic technical ideas about visual representation.
+
+"""
+
+graph_system_easy_template = """
+You are a graph generating bot. You have access to the scenario as a text in context. You need to provide the 
+mermaid graph notation as a text for the given scenario. User will provide the scenario then you need to get the best possible graph for the context.
+Just reply to the user without any formatting. Use above information as a reference to provide the best possible graph representation for the user.
+
+you have freedom to choose the best possible visualizations for the given scenario out of below options:
+
+1. Flowchart
+2. State Diagram
+3. Gantt
+4. Pie Chart
+5. Mindmaps
+6. Timeline
+7.XYChart
+8.Block Diagram
+
+consider user is not aware of the technical terms and asking for a simple visual representation for the given scenario.
 
 """
 
@@ -54,12 +114,12 @@ Scenario :
 """
 
 summary_system_template = """
-You are a summarization bot. User will provide a paragraph and you need to summarize the paragraph in a few sentences.
-All the important information should be included in the summary.
+You are a AI teacher. You are very talented in explaining the hard content easier to other. You will be provide a text to be explained by user. You need to explain it to user in a simple way to understand the context.
+Make sure all the necessary information is included in the answer. Answer should be explain in point wise format.
 """
 
 summary_human_template = """
-Paragraph :
+content to be explained :
 {para}
 """
 
@@ -73,17 +133,17 @@ templates = {
     "graph": [
         {
             "difficulty": "easy",
-            "system": graph_system_template,
+            "system": graph_system_easy_template,
             "human": graph_human_template,
         },
         {
             "difficulty": "medium",
-            "system": graph_system_template,
+            "system": graph_system_medium_template,
             "human": graph_human_template,
         },
         {
             "difficulty": "hard",
-            "system": graph_system_template,
+            "system": graph_system_hard_template,
             "human": graph_human_template,
         },
     ],
